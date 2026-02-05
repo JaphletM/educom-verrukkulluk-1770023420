c350372eab8fd07ac2cdfe8b43813db7ba0c57dc
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-02-05 08:46:38 +0100

    User and Artikel

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
 require_once("lib/database.php");
-require_once("lib/artikel.php");
+require_once("lib/user.php");
 
 /// INIT
 $db = new database();
-$art = new artikel($db->getConnection());
+$user = new user($db->getConnection());
 
 
 /// VERWERK 
-$data = $art->selecteerArtikel(2);
+$data = $user->selecteerUser(2);
 
 /// RETURN
 var_dump($data);
