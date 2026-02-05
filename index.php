d539f3865360688d4d02ae8ac1d1e12f453fbd24
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-02-05 09:30:41 +0100

    keuken_type class

--- index.php
+++ index.php
@@ -6,10 +6,15 @@ require_once("lib/user.php");
 /// INIT
 $db = new database();
 $user = new user($db->getConnection());
-
+$art=new artikel($db->getConnection());
+$keuken=new keuken_type($db->getConnection());
 
 /// VERWERK 
-$data = $user->selecteerUser(2);
+$userdata = $user->selecteerUser(2);
+$keukendata = $keuken->selecteerKeukenType(2);
+$artikeldata=$art->selecteerArtikel(2);
 
 /// RETURN
-var_dump($data);
+var_dump($userdata);
+var_dump($keuken_data);
+var_dump($artikeldata);
