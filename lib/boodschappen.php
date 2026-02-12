074fc2ee9284885c056830edef606ac5260a1b60
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-02-12 14:38:12 +0100

    geformat #12

--- lib/boodschappen.php
+++ lib/boodschappen.php
@@ -1,49 +1,66 @@
-<?php 
+<?php
 
-class boodschappen {
+class boodschappen
+{
     private $connection;
     private $user;
     private $ingredients;
 
-    public function __construct($connection){
-        $this->connection=$connection;
-        $this->user=new user($connection);
-        $this->ingredients=new ingredient($connection);
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+        $this->user = new user($connection);
+        $this->ingredients = new ingredient($connection);
 
     }
 
 
-    public function boodschappenToevoegen($gerecht_id,$user_id){
-       
-         $ingredienten = $this->ophalenIngredienten($gerecht_id);
+    public function boodschappenToevoegen($gerecht_id, $user_id)
+    {
 
-    
+        $ingredienten = $this->ophalenIngredienten($gerecht_id);
 
 
-        foreach($ingredienten as $ingredient){
-            $artikel_id=$ingredient["artikel_id"];
-            $aantal=$ingredient["aantal"];
-            
-    
-            $bestaat=$this->ArtikelOpLijst($artikel_id,$user_id);
+        $totaalVerpakkingen = 0;
 
-            if($bestaat==false){
-                    $this->artikelToevoegen($artikel_id,$user_id,$aantal);
+
+        foreach ($ingredienten as $ingredient) {
+            $artikel_id = $ingredient["artikel_id"];
+            $nodig = $ingredient["aantal"];
+            $verpakking = $ingredient["verpakking"];
+
+
+            $bestaat = $this->ArtikelOpLijst($artikel_id, $user_id);
+
+            if ($bestaat != false) {
+                $huidig = (int) $bestaat["aantal"];
+                $this->artikelBijwerken($artikel_id, $user_id, $nodig);
             } else {
-                    $this->artikelBijwerken($artikel_id, $user_id,$aantal);
+                $huidig = 0;
+                $this->artikelToevoegen($artikel_id, $user_id, $nodig);
             }
+
+            $oudePacks = (int) ceil($huidig / $verpakking);
+            $nieuwePacks = (int) ceil(($huidig + $nodig) / $verpakking);
+
+            $extraPacks = max($nieuwePacks - $oudePacks, 0);
+            $totaalVerpakkingen += $extraPacks;
+
         }
+
         return true;
+
     }
-    
-    
 
 
-    private function ArtikelOpLijst($artikel_id,$user_id){
-        $boodschappen=$this->ophalenBoodschappen($user_id);
 
-        foreach($boodschappen as $boodschap){
-            if($boodschap["artikel_id"]===$artikel_id){
+
+    private function ArtikelOpLijst($artikel_id, $user_id)
+    {
+        $boodschappen = $this->ophalenBoodschappen($user_id);
+
+        foreach ($boodschappen as $boodschap) {
+            if ($boodschap["artikel_id"] === $artikel_id) {
                 return $boodschap;
 
             }
@@ -51,61 +68,61 @@ class boodschappen {
         return false;
     }
 
-    private function ophalenBoodschappen($user_id){
-        $sql="SELECT * FROM boodschappen_lijst 
+    private function ophalenBoodschappen($user_id)
+    {
+        $sql = "SELECT * FROM boodschappen_lijst 
             WHERE user_id=$user_id";
 
-        $return=[];
+        $return = [];
 
-        $result=mysqli_query($this->connection,$sql);
-        while ($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $result = mysqli_query($this->connection, $sql);
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 
-        
-        $ingredient = $this->ingredients->selecteerIngredient($row["artikel_id"]);
-        $verpakking = $ingredient["verpakking"];
+            $return[] = $row;
+        }
+        ;
 
-        $row["packages"] = ceil($row["aantal"] / $verpakking);
-
-        $return[]=$row;
-    };
-
-    return $return;
+        return $return;
     }
 
-    private function ophalenIngredienten($gerecht_id){
+    private function ophalenIngredienten($gerecht_id)
+    {
 
         return $this->ingredients->selecteerIngredient($gerecht_id);
 
     }
 
 
-    private function artikelToevoegen($artikel_id,$user_id,$aantal){
+    private function artikelToevoegen($artikel_id, $user_id, $aantal)
+    {
 
-        
 
-        $sql="INSERT INTO boodschappen_lijst (user_id, artikel_id, aantal) 
+
+        $sql = "INSERT INTO boodschappen_lijst (user_id, artikel_id, aantal) 
             VALUES ('$user_id','$artikel_id','$aantal')";
 
-       
+
         $result = mysqli_query($this->connection, $sql);
-    
+
         return $result;
 
     }
 
-    private function artikelBijwerken($artikel_id,$user_id,$extraAantal){
-         $sql = "UPDATE boodschappen_lijst 
+    private function artikelBijwerken($artikel_id, $user_id, $extraAantal)
+    {
+        $sql = "UPDATE boodschappen_lijst 
             SET aantal = aantal + $extraAantal 
             WHERE artikel_id = $artikel_id 
             AND user_id = $user_id";
 
-    $result = mysqli_query($this->connection,$sql);
-    
-    return $result;
- 
+        $result = mysqli_query($this->connection, $sql);
+
+        return $result;
+
 
     }
 }
 
 
 ?>
+
